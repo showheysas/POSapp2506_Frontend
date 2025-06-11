a21f45f31f5585f19b32bf93ab64b47993da9ed7
showheysas <shoheysas@gmail.com>
2025-06-11 10:38:52 +0900

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  // 環境変数を明示的に公開する設定を追加
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
+
 module.exports = nextConfig;
